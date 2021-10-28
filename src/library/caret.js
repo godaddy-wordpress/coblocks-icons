76e6528ca1428b34288be9562a72f97fc7fd6fe8
Jason Lemay <info@jasonlemay.com>
2021-10-28 14:49:29 -0400

    Add caret icon (#13)
    Co-authored-by: Jason Lemay <jlemay@godaddy.com>

---
+++ src/library/caret.js
@@ -0,0 +1,19 @@
+// --
+// -- WARNING!
+// -- This is an auto-generated file. Do not edit.
+// --
+
+import { SVG, Path } from '@wordpress/primitives';
+
+const caret = (
+	<SVG fill="none" view-box="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<Path
+			fill="currentColor"
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="M19.39 14.99l-1.41 1.41L12 10.43 6.02 16.4l-1.41-1.41L12 7.6l7.39 7.39z"
+		/>
+	</SVG>
+);
+
+export default caret;
