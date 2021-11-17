b37a142eab3bb86c412d47211a574fa2e08b77ba
Olivier Lafleur <olafleur@godaddy.com>
2021-11-17 10:45:33 -0500

    New icon Timer

--- src/index.js
+++ src/index.js
@@ -85,6 +85,7 @@ export { default as ShareIcon } from './library/share';
 export { default as SocialProfilesIcon } from './library/social-profiles';
 export { default as SyncIcon } from './library/sync';
 export { default as TabletIcon } from './library/tablet';
+export { default as TimerIcon } from './library/timer';
 export { default as TrashConfirmIcon } from './library/trash-confirm';
 export { default as TrashIcon } from './library/trash';
 export { default as TwitterIcon } from './library/twitter';
